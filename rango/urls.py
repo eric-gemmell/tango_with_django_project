21d2e47992c620f068ce8aeb57beb9f87381dc91
palamalama <ericgemmell@gmail.com>
2019-01-22 08:44:46 +0000

    new test website

--- rango/urls.py
+++ rango/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from rango import views
 
 urlpatterns = [
-	url(r"^.*$",views.index,name="index")
+	url(r"^v2$",views.v2,name="v2"),
+	url(r"^$",views.index,name="index"),
 ]
--- rango/views.py
+++ rango/views.py
@@ -6,3 +6,6 @@ def index(request):
 	context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
 
 	return render(request, "rango/index.html",context=context_dict)
+
+def v2(request):
+	return render(request, "rango/v2.html")
