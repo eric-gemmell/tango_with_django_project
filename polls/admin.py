9146183b1cbcb9086f951ecf464bf97efdc5b623
palamalama <ericgemmell@gmail.com>
2019-01-24 15:55:06 +0000

    tests,not working though

--- polls/admin.py
+++ polls/admin.py
@@ -9,11 +9,14 @@ class ChoiceInline(admin.TabularInline):
 
 # Register your models here.
 class QuestionAdmin(admin.ModelAdmin):
-	fieldsets = [
-        (None,               {'fields': ['question_text']}),
+        fieldsets = [
+        (None,{'fields': ['question_text']}),
        	('Date information', {'fields': ['pub_date']}),
 	]
-	list_display = ('question_text', 'pub_date')
+        list_display = ('question_text', 'pub_date')
+        list_filter = ['pub_date']
+        search_fields = ['question_text']
+
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
