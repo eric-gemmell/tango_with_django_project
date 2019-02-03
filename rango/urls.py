2fa4de249f8dd790789d7b4688819e0c255749af
palamalama <ericgemmell@gmail.com>
2019-02-03 02:01:25 +0000

    Advancing in chapter9, NON FUNCTIONAL VERSION

--- rango/urls.py
+++ rango/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
 	url(r"admin/",admin.site.urls),
 	url(r'^add_category/$', views.add_category, name='add_category'),
 	url(r"^category/(?P<category_name_slug>[\w\-]+)/add_page/$", views.add_page, name="add_page"),
+	url(r'^register/$',views.register,name='register'),
 
 ]
